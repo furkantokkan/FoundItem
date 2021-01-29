47f852ec1ac7dc36ba9b2bb5dd9704919b1869ac
burhankanat <burhankanat2@gmail.com>
2021-01-29 23:34:13 +0300

    hepisi

---
+++ Assets/Burhan/Script/ItemFounder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFounder : MonoBehaviour
+{
+    private List<Transform> items = new List<Transform>();
+
+
+    public Transform ClosestItem()
+    {
+        items.Clear();
+
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("Item"))
+        {
+            items.Add(item.transform);
+        }
+
+        return GetClosestItem(items);
+    }
+
+    Transform GetClosestItem(List<Transform> items)
+    {
+        Transform bestTarget = null;
+        float closestDistance = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+
+        foreach (Transform potentialTarget in items)
+        {
+            Vector3 directionToTarget = potentialTarget.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+
+            if (dSqrToTarget < closestDistance)
+            {
+                closestDistance = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+
+        return bestTarget;
+    }
+
+}
