0281bc7ac7de09fdea4794e2699f6e039c00a52e
Furkan Tokkan <efurk64@gmail.com>
2021-01-29 19:02:43 +0300

    AI Spawn

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    void Start()
+    {
+        GameManager.instance.currentQueue.Add(this.gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [Header("Queue Settings")]
+    public List<GameObject> currentQueue = new List<GameObject>();
+    [Header("Client Settings")]
+    public GameObject client;
+    public Transform clientSpawnTransform;
+    public int maxClientCount = 10;
+
+    private int currentClientCount;
+    private bool canSpawnClient = true;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void Update()
+    {
+        if (canSpawnClient && currentClientCount < maxClientCount)
+        {
+            StartCoroutine(SpawnNewClient());
+            canSpawnClient = false;
+        }
+    }
+
+    public IEnumerator SpawnNewClient()
+    {
+        bool finished = false;
+
+        while (!finished)
+        {
+            if (currentClientCount == maxClientCount)
+            {
+                finished = true;
+                break;
+            }
+            yield return new WaitForSeconds(1.5f);
+            currentClientCount++;
+            Instantiate(client, clientSpawnTransform.position, Quaternion.identity);
+            yield return null;
+        }
+        canSpawnClient = true;
+    }
+}
