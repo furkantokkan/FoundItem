e96ecd7b0fe09fbec7c8931d7114b8dc3eaff0cb
Furkan Tokkan <efurk64@gmail.com>
2021-01-30 14:08:09 +0300

    Curve Follow + Spawn Object

---
+++ Assets/Scripts/FollowCurve.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCurve : MonoBehaviour
+{
+    public BezierCurve curveToFollow;
+    public float timeBetwenPoints = 1f;
+    private List<Transform> pointList = new List<Transform>();
+    private int index = 0;
+
+    private void Start()
+    {
+        for (int i = 0; i < curveToFollow.transform.childCount; i++)
+        {
+            pointList.Add(curveToFollow.transform.GetChild(i));
+        }
+
+        StartCoroutine(MoveToPoint());
+    }
+    IEnumerator MoveToPoint()
+    {
+        bool reached = false;
+        int currentIndex = GetNextIndex();
+
+        while (!reached)
+        {
+            if (Vector3.Distance(transform.position, pointList[currentIndex].transform.position) < 0.15f)
+            {
+                reached = true;
+                break;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, pointList[currentIndex].transform.position, timeBetwenPoints * Time.deltaTime);
+            yield return null;
+        }
+
+        if (currentIndex != curveToFollow.transform.childCount - 1)
+        {
+            StartCoroutine(MoveToPoint());
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+
+    }
+    public int GetNextIndex()
+    {
+        return index++;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,8 +12,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject client;
     [SerializeField] Transform clientSpawnTransform;
     public int maxClientCount = 10;
-    public float spawnRate = 2f;
+    public float clientSpawnRate = 2f;
+    [Header("Object Settings")]
+    public GameObject[] beltToSpawn;
+    public GameObject[] objectsToSpawn;
+    public int maxObjectCount = 10;
+    public float objectSpawnRate = 2f;
 
+    private List<GameObject> SpawnedObjectsList = new List<GameObject>();
+    internal int currentObjectCount;
     internal int currentClientCount;
     private bool canSpawnClient = true;
     private void Awake()
@@ -26,6 +33,10 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
+    private void Start()
+    {
+        StartCoroutine(SpawnNewObject());
     }
     void Update()
     {
@@ -47,11 +58,41 @@ public class GameManager : MonoBehaviour
                 finished = true;
                 break;
             }
-            yield return new WaitForSeconds(spawnRate);
+            yield return new WaitForSeconds(clientSpawnRate);
             currentClientCount++;
             Instantiate(client, clientSpawnTransform.position, Quaternion.identity);
             yield return null;
         }
         canSpawnClient = true;
+    }
+    public IEnumerator SpawnNewObject()
+    {
+        bool finished = false;
+
+        while (!finished)
+        {
+            if (currentObjectCount == maxObjectCount)
+            {
+                finished = true;
+                break;
+            }
+            yield return new WaitForSeconds(objectSpawnRate);
+            currentObjectCount++;
+            Transform targetParent = GetRandomBelt().transform;
+            GameObject objectClone = Instantiate(GetRandomObject(), targetParent.transform.GetChild(0).transform.position, Quaternion.identity);
+            objectClone.GetComponent<FollowCurve>().curveToFollow = targetParent.GetComponentInChildren<BezierCurve>();
+        }
+    }
+
+    public GameObject GetRandomObject()
+    {
+        int index = Random.Range(0, objectsToSpawn.Length);
+        return objectsToSpawn[index];
+    }
+
+    public GameObject GetRandomBelt()
+    {
+        int index = Random.Range(0, beltToSpawn.Length);
+        return beltToSpawn[index];
     }
 }
