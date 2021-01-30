933ef588185576560d8a88d759e730f87b6580a4
burhankanat <burhankanat2@gmail.com>
2021-01-30 14:45:42 +0300

    son

---
+++ Assets/Burhan/Script/TimerSC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerSC : MonoBehaviour
+{
+    public float time = 120f;
+    public UnityEngine.UI.Text timerText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timerText.text = time.ToString("N1");
+    }
+}
